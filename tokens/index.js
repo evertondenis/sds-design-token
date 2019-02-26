85e79e9f50e76d7fc89ec7add596458586fe1b27
Everton Denis <everton.souza@softplan.com.br>
2019-02-26 17:06:17 -0300

    added all tokens

--- tokens/index.js
+++ tokens/index.js
@@ -1,3 +1,19 @@
 import Colors from "./colors.common";
+import Animations from "./animations.common";
+import Borders from "./borders.common";
+import MediaQueries from "./media-queries.common";
+import Opacities from "./opacities.common";
+import Spacings from "./spacings.common";
+import Typography from "./typography.common";
+import Widths from "./widths.common";
 
-export { Colors };
+export {
+  Colors,
+  Animations,
+  Borders,
+  MediaQueries,
+  Opacities,
+  Spacings,
+  Typography,
+  Widths
+};
