c0fa52e86305993744a506054c86ac50fc043f46
Everton Denis <everton.souza@softplan.com.br>
2019-02-26 17:11:26 -0300

    change version and order export

--- tokens/index.js
+++ tokens/index.js
@@ -1,6 +1,6 @@
-import Colors from "./colors.common";
 import Animations from "./animations.common";
 import Borders from "./borders.common";
+import Colors from "./colors.common";
 import MediaQueries from "./media-queries.common";
 import Opacities from "./opacities.common";
 import Spacings from "./spacings.common";
@@ -8,9 +8,9 @@ import Typography from "./typography.common";
 import Widths from "./widths.common";
 
 export {
-  Colors,
   Animations,
   Borders,
+  Colors,
   MediaQueries,
   Opacities,
   Spacings,
